da7e561a5294f6e59db4aba1366a3fc15bef57e1
ym <kiyoshi.kodama@gmail.com>
2016-11-26 14:00:34 +0900

    リファクタリング

--- lib/fresh_erd.rb
+++ lib/fresh_erd.rb
@@ -2,37 +2,54 @@ require "yaml"
 require "fresh_erd/version"
 
 module FreshErd
-  def self.exec(input, output)
-    if Dir.exist?(output)
-      while(output[-1] == "/")
-        output.chop!
+  class GenerateErd
+    def exec(input, output)
+      if Dir.exist?(output)
+        while(output[-1] == "/")
+          output.chop!
+        end
+      else
+        puts "--- output: Folder not found!"
+        puts "--- Stop the output of ERD."
+        return
       end
-    else
-      puts "--- output: Folder not found!"
-      puts "--- Stop the output of ER diagram."
-      return
+
+      if File.exist?(input)
+        generate_erd_by_setting(input, output)
+      else
+        puts "--- input: File not found!"
+        puts "--- Instead, it outputs for all models."
+        generate_project_erd(output)
+      end
     end
 
-    if File.exist?(input)
+    private
+
+    def generate_erd_by_setting(input, output)
       load_yaml = YAML.load_file(input)
       diagrams = load_yaml["diagrams"]
 
       diagrams.each do |diagram_name , diagram|
+        puts "--- Generate ERD : #{diagram_name}"
+        if diagram.nil?
+          puts "--- #{diagram_name}: The models are not described!"
+          next
+        end
         models = diagram.map { |m| m }
-        output_erd(diagram_name, models, output)
+        generate_domain_erd(diagram_name, models, output)
       end
-    else
-      puts "--- input: File not found!"
-      puts "--- Instead, it outputs for all models."
-      system "bundle exec erd --title='All domain model' --filename=project --filetype=png"
     end
-  end
 
-  def self.output_erd(diagram_name = "erd", models = nil, folder = ".")
-    return if models.nil?
-    system "bundle exec erd --only='#{models.join(",")}' "\
-           "--inheritance=true "\
-           "--title='#{diagram_name}' "\
-           "--filename=#{folder}/#{diagram_name} --filetype=png"
+    def generate_domain_erd(diagram_name = "erd", models = nil, folder = ".")
+      return if models.nil?
+      system "bundle exec erd --only='#{models.join(",")}' "\
+             "--inheritance=true "\
+             "--title='#{diagram_name}' "\
+             "--filename=#{folder}/#{diagram_name} --filetype=png"
+    end
+
+    def generate_project_erd(folder)
+      system "bundle exec erd --title='All domain model' --filename=#{folder}/project --filetype=png"
+    end
   end
 end
